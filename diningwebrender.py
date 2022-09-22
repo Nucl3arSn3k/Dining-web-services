46bc11b0c490ec434bc821607b08b276a2d2541a
Tormenator1 <shredderofwheat@gmail.com>
2022-09-22 18:54:07 -0400

    Code that I have so far
    Works at rendering the webpage,will adjust as needed since the path for firefox may be different on different machines.

---
+++ diningwebrender.py
@@ -0,0 +1,36 @@
+import requests
+from xml.etree import ElementTree as et
+from selenium import webdriver
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+
+from lxml import html
+from selenium.webdriver.firefox.options import Options
+
+#
+def main():
+    optionsv2 = Options()
+    optionsv2.add_argument("--headless")
+    url = "https://dining.rochester.edu/menu-hours/"
+
+    # binary = FirefoxBinary("C:\Program Files\Mozilla Firefox\firefox.exe")
+    browser = webdriver.Firefox(
+        executable_path="C:\geckodriver-v0.31.0-win64\geckodriver.exe",
+        options=optionsv2,
+    )
+
+    # driver = webdriver.Firefox(options=optionsv2)
+    browser.get(url)
+    page_source = browser.page_source
+
+    tree = html.fromstring(page_source)
+    # quotes = tree.xpath()
+    # print("hey")
+    print(type(tree))
+    """
+    with open("htmltxt.txt", "w+") as f:
+        f.write(var)
+    """
+
+
+if __name__ == "__main__":
+    main()
